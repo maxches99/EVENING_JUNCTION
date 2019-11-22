09689afa6eaf5b45eb3184bbb8c6dbce2c0f4c85
Max Chesnikov <maxches99@icloud.com>
2019-11-23 01:45:45 +0300

    Initial Commit

---
+++ Game_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game_test
+//
+//  Created by Максим Чесников on 23.11.2019.
+//  Copyright © 2019 Максим Чесников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
